1c269ffba7eaccbfb18608e567bec2f03e420bdb
almaroqy <bahrihussein65@gmail.com>
2023-02-09 15:55:24 +0700

    tampilan home page

---
+++ tubes_web_catalog/index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Catalog</title>
+
+    <link rel="stylesheet" href="css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav></nav>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="name_web">
+                        <a href="index.php" style="text-decoration: none;">
+                            <h1>BOOKSTRO</h1><a href="" style="text-decoration: none;">
+                                <h4>Book Store Roqy</h4>
+                            </a>
+                        </a>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="desc_web">
+                        <p>Tempat anda menemukan berbagai
+                            macam jenis buku yang anda butuhkan,
+                            dengan berbagai macam topik yang bervarian.
+                            Semakin anda lebih dalam menjelajah,
+                            semakin banyak jendela dunia yang terbuka. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="var_book">
+                        <h1 style="font-weight: 650; font-size:45px;">JENIS - JENIS BUKU</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Dewasa</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Anak</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Agama</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Umum</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Kesehatan</p>
+                        </div>
+                    </a>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Dewasa</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Anak</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Agama</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Umum</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Kesehatan</p>
+                        </div>
+                    </a>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Dewasa</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Anak</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Agama</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Umum</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col">
+                    <a href="" style="text-decoration: none;">
+                        <div class="var_book_list">
+                            <p>Kesehatan</p>
+                        </div>
+                    </a>
+
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
