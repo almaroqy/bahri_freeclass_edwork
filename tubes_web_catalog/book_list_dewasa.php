c84be0ffc8a7f89d50b945f6c11d01456502fab2
almaroqy <bahrihussein65@gmail.com>
2023-02-10 07:46:21 +0700

    ganti nama book_list jadi book_list_dewasa

---
+++ tubes_web_catalog/book_list_dewasa.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BOOSTRO - Book List</title>
+
+    <link rel="stylesheet" href="css/book_list.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="name_web">
+                        <a href="home.php" style="text-decoration: none;">
+                            <h1>BOOKSTRO</h1><a href="" style="text-decoration: none;">
+                                <h4>Book Store Roqy</h4>
+                            </a>
+                        </a>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <header></header>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
