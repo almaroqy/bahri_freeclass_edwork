084d2a401fe33ad6e9c5cec5ce8827f4f1d167ab
almaroqy <bahrihussein65@gmail.com>
2023-12-23 09:03:48 +0700

    book list dewasa done

--- tubes_web_catalog/book_list_dewasa.php
+++ tubes_web_catalog/book_list_dewasa.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BOOSTRO - Book List</title>
+    <title>BOOSTRO - Adult Book</title>
 
     <link rel="stylesheet" href="css/book_list_dewasa.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -25,15 +25,40 @@
                     </div>
                 </div>
                 <div class="col">
-                    <div class="">
-
+                    <div class="navig">
+                        <ul>
+                            <li><a href="home.php">Home</a></li>
+                            <li><a href="home.php">Home</a></li>
+                            <li><a href="home.php">Home</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
     </nav>
     <header></header>
-    <main></main>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h2>DAFTAR BUKU</h2>
+
+                </div>
+            </div>
+            <div class="row_book">
+                <div class="book_c">
+                    <img src="../tubes_web_catalog/referensi/contoh_cover_buku.png" alt="">
+                    <div class="book_desc">
+                        <p>Judul Buku : Pentingnya Edukasi Bahaya Free Sex Sejak Dini</p>
+                        <p>Genre : Slice of Life</p>
+                        <p>Penulis : Desu</p>
+                        <p>Jumlah Copy : 20</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </main>
     <footer></footer>
 </body>
 
